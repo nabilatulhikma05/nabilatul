7e73f0e4db054e97a8a21622acccd11c2f61fe92
nabilatulhikma05 <55224785+nabilatulhikma05@users.noreply.github.com>
2019-09-12 00:33:03 -0700

    Add files via upload

---
+++ program3 c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void tambah (int *angka) {
+	*angka +=20;
+}
+
+int main() {
+int nilai = 10;
+cout<<"Nilai variabel niliai adalah = "<<nilai<<endl;
+tambah(&nilai);
+cout<<"Nilai variabel nilai adalah = "<<nilai<<endl;
+return 0;
+}
